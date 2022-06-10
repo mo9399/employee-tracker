c0eb71fdcd667249f6805ebca22b2679e7afcba0
your name <you@example.com>
2022-06-10 00:01:45 -0700

    Create Users Prompts

---
+++ db/index.js
@@ -0,0 +1,51 @@
+const inquirer = require("inquirer");
+const { allowedNodeEnvironmentFlags, exit } = require("process");
+
+const userOptions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "options",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+      },
+    ])
+    .then((userChoice) => {
+      switch (userChoice.userOptions) {
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployeeRole();
+          break;
+        default:
+          exit();
+      }
+    });
+};
+
+userOptions();
